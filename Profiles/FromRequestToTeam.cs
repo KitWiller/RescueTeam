6a550427a5afb2c15246d270e07be5c295af46e7
KitWiller <81360843+KitWiller@users.noreply.github.com>
2023-07-01 14:47:42 +0200

    aa

--- Profiles/FromRequestToTeam.cs
+++ Profiles/FromRequestToTeam.cs
@@ -1,6 +1,22 @@
-﻿namespace RescueTeam.Profiles
+﻿using AutoMapper;
+using RescueTeam.Models.Team;
+
+namespace RescueTeam.Profiles
 {
-    public class FromRequestToTeam
+    public class FromRequestToTeam: Profile
     {
+
+        public FromRequestToTeam()
+        {
+            CreateMap<TeamPostRequest,DAL.Entities.Team>()
+                .ForMember(dest => dest.TeamMembers, opt => opt.Ignore()) // Ignora la mappatura di TeamMembers
+                .ForMember(dest => dest.Trasport, opt => opt.MapFrom(src => src.Trasport));
+
+            CreateMap<TeamPostRequest, DAL.Entities.Team>()
+                .ForMember(dest => dest.TeamMembers, opt => opt.Ignore()) // Ignora la mappatura di TeamMembers
+                .ForMember(dest => dest.Trasport, opt => opt.MapFrom(src => src.Trasport));
+        }
+        
+      
     }
 }
